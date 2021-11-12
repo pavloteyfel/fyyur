070fe2b61d71509a417569beb1061d87f5cca1bb
pavloteyfel <pavloteyfel@gmail.com>
2021-11-12 22:04:29 +0100

    small change

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = '<db_connection_parameters>'
 
 # Don't track modifications of objects and emit signals.
 SQLALCHEMY_TRACK_MODIFICATIONS = False
