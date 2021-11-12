db3e9040fede890740648556921a7277b6d8c44d
pavloteyfel <pavloteyfel@gmail.com>
2021-11-12 20:09:41 +0100

    styling

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+
+
 SECRET_KEY = os.urandom(32)
+
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -9,6 +12,5 @@ DEBUG = True
 # Connect to the database
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/fyyur'
 
-# If set to True, Flask-SQLAlchemy will track modifications
-# of objects and emit signals.
+# Don't track modifications of objects and emit signals.
 SQLALCHEMY_TRACK_MODIFICATIONS = False
