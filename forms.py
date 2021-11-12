1d1e38778ec4056c418c5d7fde4a1a3d36b8cec2
pavloteyfel <pavloteyfel@gmail.com>
2021-11-12 21:24:47 +0100

    enums added to forms

--- forms.py
+++ forms.py
@@ -1,84 +1,8 @@
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
 from wtforms.validators import DataRequired, URL, Optional, Regexp
-from datetime import datetime
 from flask_wtf import FlaskForm as Form
-
-
-genres = [
-    ('Alternative', 'Alternative'),
-    ('Blues', 'Blues'),
-    ('Classical', 'Classical'),
-    ('Country', 'Country'),
-    ('Electronic', 'Electronic'),
-    ('Folk', 'Folk'),
-    ('Funk', 'Funk'),
-    ('Hip-Hop', 'Hip-Hop'),
-    ('Heavy Metal', 'Heavy Metal'),
-    ('Instrumental', 'Instrumental'),
-    ('Jazz', 'Jazz'),
-    ('Musical Theatre', 'Musical Theatre'),
-    ('Pop', 'Pop'),
-    ('Punk', 'Punk'),
-    ('R&B', 'R&B'),
-    ('Reggae', 'Reggae'),
-    ('Rock n Roll', 'Rock n Roll'),
-    ('Soul', 'Soul'),
-    ('Other', 'Other'),
-]
-
-states = [
-    ('AL', 'AL'),
-    ('AK', 'AK'),
-    ('AZ', 'AZ'),
-    ('AR', 'AR'),
-    ('CA', 'CA'),
-    ('CO', 'CO'),
-    ('CT', 'CT'),
-    ('DE', 'DE'),
-    ('DC', 'DC'),
-    ('FL', 'FL'),
-    ('GA', 'GA'),
-    ('HI', 'HI'),
-    ('ID', 'ID'),
-    ('IL', 'IL'),
-    ('IN', 'IN'),
-    ('IA', 'IA'),
-    ('KS', 'KS'),
-    ('KY', 'KY'),
-    ('LA', 'LA'),
-    ('ME', 'ME'),
-    ('MT', 'MT'),
-    ('NE', 'NE'),
-    ('NV', 'NV'),
-    ('NH', 'NH'),
-    ('NJ', 'NJ'),
-    ('NM', 'NM'),
-    ('NY', 'NY'),
-    ('NC', 'NC'),
-    ('ND', 'ND'),
-    ('OH', 'OH'),
-    ('OK', 'OK'),
-    ('OR', 'OR'),
-    ('MD', 'MD'),
-    ('MA', 'MA'),
-    ('MI', 'MI'),
-    ('MN', 'MN'),
-    ('MS', 'MS'),
-    ('MO', 'MO'),
-    ('PA', 'PA'),
-    ('RI', 'RI'),
-    ('SC', 'SC'),
-    ('SD', 'SD'),
-    ('TN', 'TN'),
-    ('TX', 'TX'),
-    ('UT', 'UT'),
-    ('VT', 'VT'),
-    ('VA', 'VA'),
-    ('WA', 'WA'),
-    ('WV', 'WV'),
-    ('WI', 'WI'),
-    ('WY', 'WY'),
-]
+from enums import Genre, State
+from datetime import datetime
 
 
 class ShowForm(Form):
@@ -96,7 +20,7 @@ class VenueForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=states
+        choices=State.choices()
     )
     address = StringField(
         'address', validators=[DataRequired()]
@@ -112,7 +36,7 @@ class VenueForm(Form):
     )
     genres = SelectMultipleField(
         'genres', validators=[DataRequired()],
-        choices=genres
+        choices=Genre.choices()
     )
     facebook_link = StringField(
         'Facebook Link', validators=[URL(message='Invalid Facebook Link'), 
@@ -135,7 +59,7 @@ class ArtistForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=states
+        choices=State.choices()
     )
     phone = StringField(
         'phone', validators=[DataRequired(), 
@@ -148,7 +72,7 @@ class ArtistForm(Form):
     )
     genres = SelectMultipleField(
         'genres', validators=[DataRequired()],
-        choices=genres
+        choices=Genre.choices()
      )
     facebook_link = StringField(
         'facebook_link', validators=[URL(message='Invalid Facebook Link'), 
