1b10c30c221d9915a5239160154f499c0134a074
pavloteyfel <pavloteyfel@gmail.com>
2022-01-09 22:11:50 +0100

    feature: Add load params from env

--- config.py
+++ config.py
@@ -1,19 +1,23 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-SECRET_KEY = os.urandom(32)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # CSRF protection configuration
-WTF_CSRF_ENABLED = True
+WTF_CSRF_ENABLED = os.environ.get("WTF_CSRF_ENABLED", False) == "true"
 
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False) == "true"
 
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
 # Don't track modifications of objects and emit signals.
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_TRACK_MODIFICATIONS = (
+    os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS", False) == "true"
+)
